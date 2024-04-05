38a66ad860f2403a42279b06830da13453c291a7
suyash <sonarsuyash2@gmail.com>
2024-04-05 21:00:37 +0530

    test-deploy

--- Medimute-Backend/server.js
+++ Medimute-Backend/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const path = require("path")
 
 const socketServer = require("./socketServer");
 const authRoutes = require("./routes/authRoutes");
@@ -17,6 +18,20 @@ app.use(cors());
 // register the routes
 app.use("/api/auth", authRoutes);
 app.use("/api/friend-invitation", friendInvitationRoutes);
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname1, "/Medimute-Frontend/build")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "Medimute-Frontend", "build", "index.html"));
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running successfully!!");
+  })
+}
 
 const server = http.createServer(app);
 socketServer.registerSocketServer(server);
